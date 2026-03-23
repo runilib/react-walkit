99faba52694bed9553e0a5b11a0e366defe76a6f
Aklade Kouassi Sosthene <aklade-kouassi-sosthene.adandohoin-ext@socgen.com>
2026-03-23 14:01:41 +0100

    fix problems

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+    "index.native": "src/index.native.ts",
+  },
+  format: ["esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  treeshake: true,
+  target: "es2019",
+  external: ["react", "react-dom", "react-native", "react-native-web"],
+  tsconfig: "./tsconfig-build.json",
+});
